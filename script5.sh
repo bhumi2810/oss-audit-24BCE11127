a3f98468a21bc98d6d7ba0bde67bf926df5bffb4
bhumi2810 <bhumi.agarwal.2810mpspn@gmail.com>
2026-03-31 10:48:19 +0530

    Update script5.sh

--- script5.sh
+++ script5.sh
@@ -2,15 +2,17 @@
 
 #ask user the questions you want to ask and then save them in a text file
 echo "Enter the answers of following questions"
-read -p "What is your favourite open source language?"OPEN
-read -p "Why  is it your favourite ?"FAV
-read -p " How many projects have you made?"PRO
+read -p "What is your favourite open source language?" OPEN
+read -p "Why  is it your favourite ?" FAV
+read -p " How many projects have you made?" PRO
 #now answer these questions but using the questions you used as a variable
 echo "My favourite open source language is $OPEN."
-echo "It is my favourite because its #FAV to learn"
+echo "It is my favourite because its $FAV to learn"
 echo "I have made $PRO  in it"
 #now save all of it to the  text file
+echo "My favourite open source language is $OPEN. It is my favourite because its $FAV to learn.I have made $PRO  in it"> manifesto.txt
 echo "Saving all of this to text file"
 #display the file you made just now
 cat manifesto.txt
+
 
